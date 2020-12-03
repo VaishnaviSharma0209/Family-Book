cb10ce7efc9252beb10b4cf6560fc066c0c82186
VaishnaviSharma0209 <72790064+VaishnaviSharma0209@users.noreply.github.com>
2020-12-03 15:08:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+var picture=[
+    "Me.jpg",
+    "Paru.jpg",
+    "Reshmi.jpg",
+    "Hari.jpg",
+    "Prabha.JPG",
+    "VKC.jpg",
+    "me1.jpg",
+    "Paru1.JPG"
+];
+var name=[
+    "Vaishnavi Sharma (Me)",
+    "Shivani Sharma (Sister)",
+    "Reshmi Cp (Mother)",
+    "Hari (Father)",
+    "Prabha (Grand Mother)",
+    "VK Chandrashekran (Grand Father)",
+    "My First Birthday ",
+    "My Sister's Frist Birthday"
+];
+var i=0;
+function Nextpic(){
+    document.getElementById("people").innerHTML=name[i];
+    document.getElementById("Image").src=picture[i];
+    i++
+    if (i==7){
+        i=0
+    }
+}
